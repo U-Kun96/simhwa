52f6fd14aca4358ae8d7bfa5ce998ae4970a9d82
유민식 <soule125@naver.com>
2024-02-21 14:35:39 +0900

    리줌 컨트롤러 이력서 목록 조회  작성

--- src/controllers/resume.controller.js
+++ src/controllers/resume.controller.js
@@ -1,8 +1,8 @@
-import { resumeService } from "../services/resume.service.js"; // 객체일 때 {} = 밸류값만
+// import { resumeService } from "../services/resume.service.js"; // 객체일 때 {} = 밸류값만
 export class resumeController {
-  //   constructor(resumeService) {
-  //     this.resumeService = resumeService;
-  //   }
+  constructor(resumeService) {
+    this.resumeService = resumeService;
+  }
 
   // 이력서 생성
   creatResume = async (req, res, next) => {
@@ -13,6 +13,17 @@ export class resumeController {
       const createdResume = await this.resumeController.creatResume(userId, title, content, status);
 
       return res.status(201).json({ createdResume });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  // 이력서 목록 조회
+  findResumes = async (req, res, next) => {
+    try {
+      const resumes = await this.resumeController.findResumes();
+
+      return res.status(200).json({ resumes });
     } catch (err) {
       next(err);
     }
